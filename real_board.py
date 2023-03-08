1bf2d4d05adf7b5eaa754998923c46881d955792
Johnny-K1 <jkosecky54@gmail.com>
2023-03-08 15:43:53 +0100

    for Peto

--- real_board.py
+++ real_board.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((5*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:5].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
@@ -15,11 +15,13 @@ images = glob.glob('*.jpg')
 
 for fname in images:
     img = cv.imread(fname)
-    gray_image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    plt.imshow(gray_image, cmap="gray", vmin=0, vmax=255)
-    plt.show()
+    cropped_image = img[75:475, 100:600]
+    gray_image = cv.cvtColor(cropped_image, cv.COLOR_BGR2GRAY)
+
+    # plt.imshow(cropped_image, cmap="gray", vmin=0, vmax=255)
+    # plt.show()
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray_image, (8, 6), None)
+    ret, corners = cv.findChessboardCorners(gray_image, (7, 5), None)
     # If found, add object points, image points (after refining them)
     if ret == True:
         print("HEj")
@@ -27,8 +29,12 @@ for fname in images:
         corners2 = cv.cornerSubPix(gray_image, corners, (11, 11), (-1, -1), criteria)
         imgpoints.append(corners2)
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (7, 6), corners2, ret)
-        cv.imshow('img', img)
+        cv.drawChessboardCorners(cropped_image, (7, 5), corners2, ret)
+        cv.imshow('img', cropped_image)
         cv.waitKey(500)
 
+        ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray_image.shape[::-1], None, None)
+        print(mtx)
+
+        cv.waitKey(0)
 cv.destroyAllWindows()
